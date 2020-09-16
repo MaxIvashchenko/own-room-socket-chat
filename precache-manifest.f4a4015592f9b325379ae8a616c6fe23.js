5751243b3be97d6392e3ee57e3d9961f33914685
Ivashchenko Maksym <maksym.entex@gmail.com>
2020-09-16 17:18:07 +0300

    Updates

---
+++ precache-manifest.f4a4015592f9b325379ae8a616c6fe23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5479716ab9fb8cafd4707d3ea1073bc",
+    "url": "/own-room-socket-chat/index.html"
+  },
+  {
+    "revision": "a7d70a6fe08304b7a71b",
+    "url": "/own-room-socket-chat/static/css/main.fdce3e96.chunk.css"
+  },
+  {
+    "revision": "3b8d9bc94776a17bfcdc",
+    "url": "/own-room-socket-chat/static/js/2.49dec368.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/own-room-socket-chat/static/js/2.49dec368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7d70a6fe08304b7a71b",
+    "url": "/own-room-socket-chat/static/js/main.3ae5fb2a.chunk.js"
+  },
+  {
+    "revision": "1bd5c56d8bc6925d1247",
+    "url": "/own-room-socket-chat/static/js/runtime-main.0d471d43.js"
+  }
+]);
